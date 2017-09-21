d8721c63b7da6b9e584d0174819c430416fd6aaa
Shiva Patel <shivapatel15@yahoo.com>
2017-09-21 12:56:14 -0400

    normal push

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
+const session = require('express-session');
+const config = require('./config');
 // app declaration
 const app = express();
 
 // middleware
 app.use(bodyParser.json());
+
+// const options = {
+//     origin: 'http://localhost:8080'
+// }
+
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/public/'));
+
+app.use(session({
+    secret,
+    saveUninitialized: true,
+    resave: true
+}));
+
 app.use(logger((token, req, res) => {
     console.log('BODY: ', req.body);
     console.log('QUERY: ', req.query);
     console.log('PARAMS: ',  req.params);
+    console.log('HEADERS: ', req.headers);
+    console.log('SESSION: ', req.session);
 }));
 
 // controllers
@@ -30,6 +50,16 @@ app.put('/api/team', winnersCtrl.deleteTeam);
 
 app.delete('/api/team/:id', winnersCtrl.deleteTeam);
 
+app.get('/api/bracket', (req, res, next) => {
+    res.json(req.session.bracket);
+});
+
+app.post('/api/bracket', (req, res, next) => {
+    if (!req.session.bracket) req.session.bracket = [];
+    req.session.bracket.push(req.body);
+    res.json(req.session.bracket);
+});
+
 function isAuthed(req, res, next) {
     if (true) {
         res.status(403).json('NOT AUTHORIZED');
@@ -44,5 +74,3 @@ function isAuthed(req, res, next) {
 app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
-
-
